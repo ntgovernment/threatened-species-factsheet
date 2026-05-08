7d04cc4d5a62a9b27f8aa5fd227e3f4b460d93b9
Nithya Yamasinghe <Nithya.Yamasinghe@nt.gov.au>
2026-05-08 11:54:08 +0930

    bugfix hide caption when there's no images

--- dist/threatened-species-factsheet.js
+++ dist/threatened-species-factsheet.js
@@ -31001,7 +31001,7 @@
       <div class="sidebar-image-container">
         <div class="fotorama" data-auto="false" data-loop="true" data-keyboard="true" data-nav="thumbs">
           <img src="${e1}" alt="${t}" loading="lazy"
-               onerror="this.parentElement.parentElement.style.display='none'">
+               onerror="this.parentElement.parentElement.parentElement.style.display='none'">
           <img src="${e2}" alt="${t}" loading="lazy"
                onerror="this.parentElement.style.display='none'">
         </div>
