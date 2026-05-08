2ee4797c141635d22235de352d841e18e4836013
Nithya Yamasinghe <Nithya.Yamasinghe@nt.gov.au>
2026-05-08 10:58:50 +0930

    added navigation thumbnails

--- dist/threatened-species-factsheet.js
+++ dist/threatened-species-factsheet.js
@@ -30999,7 +30999,7 @@
             return `
     <figure class="sidebar-image mb-4">
       <div class="sidebar-image-container">
-        <div class="fotorama" data-auto="false" data-loop="true" data-keyboard="true">
+        <div class="fotorama" data-auto="false" data-loop="true" data-keyboard="true" data-nav="thumbs">
           <img src="${e1}" alt="${t}" loading="lazy"
                onerror="this.parentElement.parentElement.style.display='none'">
           <img src="${e2}" alt="${t}" loading="lazy"
